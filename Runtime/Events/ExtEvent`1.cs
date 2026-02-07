b7e829553673085cfe567806de97bf774f890742
theqoxxy <143907842+theqoxxy@users.noreply.github.com>
2026-02-07 19:54:22 +0100

    Update ExtEvent`1.cs

--- Runtime/Events/ExtEvent`1.cs
+++ Runtime/Events/ExtEvent`1.cs
@@ -1,70 +1,48 @@
 ﻿namespace ExtEvents
 {
     using System;
-    using System.Runtime.CompilerServices;
     using JetBrains.Annotations;
 
     [Serializable]
-    public class ExtEvent<T> : BaseExtEvent
+    public class ExtEvent<T> : ExtEventBase<Action<T>>
     {
         private readonly unsafe void*[] _arguments = new void*[1];
+        protected override unsafe void*[] Arguments => _arguments;
 
         private Type[] _eventParamTypes;
         protected override Type[] EventParamTypes => _eventParamTypes ??= new Type[] { typeof(T) };
 
-        /// <summary>
-        /// The dynamic listeners list that you can add your listener to.
-        /// </summary>
         [PublicAPI]
         public event Action<T> DynamicListeners;
-
-        internal override Delegate _dynamicListeners => DynamicListeners;
-
-        /// <summary>
-        /// Invokes all listeners of the event.
-        /// </summary>
-        [PublicAPI]
-        public void Invoke(T arg)
+        
+        protected override Action<T> DynamicListenersField
         {
+            get => DynamicListeners;
+            set => DynamicListeners = value;
+        }
+
+        protected override void PrepareArguments(params object[] args)
+        {
+
+            if (args.Length != 1 || args[0] is not T)
+                throw new ArgumentException($"Expected 1 argument of type {typeof(T).Name}");
+            
             unsafe
             {
-                _arguments[0] = Unsafe.AsPointer(ref arg);
-
-                // ReSharper disable once ForCanBeConvertedToForeach
-                for (int index = 0; index < _persistentListeners.Length; index++)
-                {
-                    _persistentListeners[index].Invoke(_arguments);
-                }
+                T typedArg = (T)args[0];
+                _arguments[0] = UnsafeHelper.AsPointer(ref typedArg);
             }
+        }
 
+        protected override void InvokeDynamicListeners(params object[] args)
+        {
+            if (args.Length != 1 || args[0] is not T arg)
+                throw new ArgumentException($"Expected 1 argument of type {typeof(T).Name}");
+            
             DynamicListeners?.Invoke(arg);
         }
 
         [PublicAPI]
-        public void AddListener(Action<T> listener) => DynamicListeners += listener;
-
-        [PublicAPI]
-        public void RemoveListener(Action<T> listener) => DynamicListeners -= listener;
-
-        [PublicAPI]
-        public void RemoveAllListeners() => DynamicListeners = null;
-
-        public static ExtEvent<T> operator +(ExtEvent<T> extEvent, Action<T> listener)
-        {
-            if (extEvent == null)
-                return null;
-
-            extEvent.AddListener(listener);
-            return extEvent;
-        }
-
-        public static ExtEvent<T> operator -(ExtEvent<T> extEvent, Action<T> listener)
-        {
-            if (extEvent == null)
-                return null;
-
-            extEvent.RemoveListener(listener);
-            return extEvent;
-        }
+        public void Invoke(T arg) => InvokeInternal(arg);
     }
 }
