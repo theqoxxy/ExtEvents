aeafc0dbdd84af7997ad3e9cfb273c09beb3d04e
theqoxxy <143907842+theqoxxy@users.noreply.github.com>
2026-02-07 19:54:09 +0100

    Update ExtEvent.cs

--- Runtime/Events/ExtEvent.cs
+++ Runtime/Events/ExtEvent.cs
@@ -4,60 +4,39 @@ namespace ExtEvents
     using JetBrains.Annotations;
 
     [Serializable]
-    public class ExtEvent : BaseExtEvent
+    public class ExtEvent : ExtEventBase<Action>
     {
         protected override Type[] EventParamTypes => Type.EmptyTypes;
+        
+        protected override unsafe void*[] Arguments => null;
 
-        /// <summary>
-        /// The dynamic listeners list that you can add your listener to.
-        /// </summary>
         [PublicAPI]
         public event Action DynamicListeners;
-        internal override Delegate _dynamicListeners => DynamicListeners;
+        
+        protected override Action DynamicListenersField
+        {
+            get => DynamicListeners;
+            set => DynamicListeners = value;
+        }
+
+        protected override void PrepareArguments(params object[] args)
+        {
+            if (args.Length != 0)
+                throw new ArgumentException("ExtEvent without parameters expects no arguments");
+        }
+
+        protected override void InvokeDynamicListeners(params object[] args)
+        {
+            if (args.Length != 0)
+                throw new ArgumentException("ExtEvent without parameters expects no arguments");
+            
+            DynamicListeners?.Invoke();
+        }
 
         /// <summary>
         /// Invokes all listeners of the event.
         /// </summary>
         [PublicAPI]
-        public void Invoke()
-        {
-            unsafe
-            {
-                // ReSharper disable once ForCanBeConvertedToForeach
-                for (int index = 0; index < _persistentListeners.Length; index++)
-                {
-                    _persistentListeners[index].Invoke(null);
-                }
-            }
-
-            DynamicListeners?.Invoke();
-        }
-
-        [PublicAPI]
-        public void AddListener(Action listener) => DynamicListeners += listener;
-
-        [PublicAPI]
-        public void RemoveListener(Action listener) => DynamicListeners -= listener;
-
-        [PublicAPI]
-        public void RemoveAllListeners() => DynamicListeners = null;
-
-        public static ExtEvent operator +(ExtEvent extEvent, Action listener)
-        {
-            if (extEvent == null)
-                return null;
-
-            extEvent.AddListener(listener);
-            return extEvent;
-        }
-
-        public static ExtEvent operator -(ExtEvent extEvent, Action listener)
-        {
-            if (extEvent == null)
-                return null;
-
-            extEvent.RemoveListener(listener);
-            return extEvent;
-        }
+        public void Invoke() => InvokeInternal();
     }
 }
