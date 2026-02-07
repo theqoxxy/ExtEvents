1898481bdaa773b63eff66ee2e48206a320db7d1
theqoxxy <143907842+theqoxxy@users.noreply.github.com>
2026-02-07 19:54:47 +0100

    Update ExtEvent`3.cs

--- Runtime/Events/ExtEvent`3.cs
+++ Runtime/Events/ExtEvent`3.cs
@@ -1,71 +1,51 @@
 namespace ExtEvents
 {
     using System;
-    using System.Runtime.CompilerServices;
     using JetBrains.Annotations;
 
     [Serializable]
-    public class ExtEvent<T1, T2, T3> : BaseExtEvent
+    public class ExtEvent<T1, T2, T3> : ExtEventBase<Action<T1, T2, T3>>
     {
         private readonly unsafe void*[] _arguments = new void*[3];
+        protected override unsafe void*[] Arguments => _arguments;
 
         private Type[] _eventParamTypes;
         protected override Type[] EventParamTypes => _eventParamTypes ??= new Type[] { typeof(T1), typeof(T2), typeof(T3) };
 
-        /// <summary>
-        /// The dynamic listeners list that you can add your listener to.
-        /// </summary>
         [PublicAPI]
         public event Action<T1, T2, T3> DynamicListeners;
-        internal override Delegate _dynamicListeners => DynamicListeners;
-
-        /// <summary>
-        /// Invokes all listeners of the event.
-        /// </summary>
-        [PublicAPI]
-        public void Invoke(T1 arg1, T2 arg2, T3 arg3)
+        
+        protected override Action<T1, T2, T3> DynamicListenersField
         {
+            get => DynamicListeners;
+            set => DynamicListeners = value;
+        }
+
+        protected override void PrepareArguments(params object[] args)
+        {
+            if (args.Length != 3 || args[0] is not T1 || args[1] is not T2 || args[2] is not T3)
+                throw new ArgumentException($"Expected 3 arguments of types {typeof(T1).Name}, {typeof(T2).Name}, {typeof(T3).Name}");
+            
             unsafe
             {
-                _arguments[0] = Unsafe.AsPointer(ref arg1);
-                _arguments[1] = Unsafe.AsPointer(ref arg2);
-                _arguments[2] = Unsafe.AsPointer(ref arg3);
-
-                // ReSharper disable once ForCanBeConvertedToForeach
-                for (int index = 0; index < _persistentListeners.Length; index++)
-                {
-                    _persistentListeners[index].Invoke(_arguments);
-                }
+                T1 arg1 = (T1)args[0];
+                T2 arg2 = (T2)args[1];
+                T3 arg3 = (T3)args[2];
+                _arguments[0] = UnsafeHelper.AsPointer(ref arg1);
+                _arguments[1] = UnsafeHelper.AsPointer(ref arg2);
+                _arguments[2] = UnsafeHelper.AsPointer(ref arg3);
             }
-
-            DynamicListeners?.Invoke(arg1, arg2, arg3);
         }
 
-        [PublicAPI]
-        public void AddListener(Action<T1, T2, T3> listener) => DynamicListeners += listener;
-
-        [PublicAPI]
-        public void RemoveListener(Action<T1, T2, T3> listener) => DynamicListeners -= listener;
-
-        [PublicAPI]
-        public void RemoveAllListeners() => DynamicListeners = null;
-
-        public static ExtEvent<T1, T2, T3> operator +(ExtEvent<T1, T2, T3> extEvent, Action<T1, T2, T3> listener)
+        protected override void InvokeDynamicListeners(params object[] args)
         {
-            if (extEvent == null)
-                return null;
-
-            extEvent.AddListener(listener);
-            return extEvent;
+            if (args.Length != 3 || args[0] is not T1 || args[1] is not T2 || args[2] is not T3)
+                throw new ArgumentException($"Expected 3 arguments of types {typeof(T1).Name}, {typeof(T2).Name}, {typeof(T3).Name}");
+            
+            DynamicListeners?.Invoke((T1)args[0], (T2)args[1], (T3)args[2]);
         }
 
-        public static ExtEvent<T1, T2, T3> operator -(ExtEvent<T1, T2, T3> extEvent, Action<T1, T2, T3> listener)
-        {
-            if (extEvent == null)
-                return null;
-
-            extEvent.RemoveListener(listener);
-            return extEvent;
-        }
+        [PublicAPI]
+        public void Invoke(T1 arg1, T2 arg2, T3 arg3) => InvokeInternal(arg1, arg2, arg3);
     }
 }
